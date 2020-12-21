e02a5d5e61d0d558e000e73364cda41778488214
He-commitsAA <60632165+He-commitsAA@users.noreply.github.com>
2020-12-21 12:35:02 -0600

    Add files via upload

---
+++ SmoothstackJavaAssignments/EndofWeek_1/FunctionalTest.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package com.ss.sf.williamtraining.javaendweekone;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * @author William
+ * 
+ * Tests Functional.java
+ *
+ */
+public class FunctionalTest {
+	
+	/*
+	 * Tests rightDigitTest()
+	 */
+	@Test
+	public void rightDigitTest() {
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1, 2, 3)),Functional.rightDigit(new ArrayList<Integer>(Arrays.asList(1, 12, 113))));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0, 0, 0)),Functional.rightDigit(new ArrayList<Integer>(Arrays.asList(0, 10000, 11000000))));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(1, 1, 1)),Functional.rightDigit(new ArrayList<Integer>(Arrays.asList(221801, 618741, 3424321))));
+	}
+	
+	/*
+	 * Tests doubling()
+	 */
+	@Test
+	public void doublingTest() {
+		assertEquals(new ArrayList<Integer>(Arrays.asList(20, 200, 2000)),Functional.doubling(new ArrayList<Integer>(Arrays.asList(10, 100, 1000))));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(0, 0, 0)),Functional.doubling(new ArrayList<Integer>(Arrays.asList(0, 0, 0))));
+		assertEquals(new ArrayList<Integer>(Arrays.asList(70, -50, 200)),Functional.doubling(new ArrayList<Integer>(Arrays.asList(35, -25, 100))));
+	}
+	
+	/*
+	 * Tests noX()
+	 */
+	
+	@Test
+	public void noXTest() {
+		assertEquals(new ArrayList<>(Arrays.asList("[a]","[b]","[]")),Functional.noX(new ArrayList<String>(Arrays.asList("ax", "bx", "x"))));
+		assertEquals(new ArrayList<>(Arrays.asList("[y]","[b]","[]")),Functional.noX(new ArrayList<String>(Arrays.asList("xy", "bx", "x"))));
+		assertNotEquals(new ArrayList<>(Arrays.asList("[x]","[x]","[x]")),Functional.noX(new ArrayList<String>(Arrays.asList("x", "x", "x"))));
+	}
+	
+
+}
